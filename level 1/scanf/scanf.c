60cb1a7c66ce89b77daea4e2456781799424595b
Lubnabd <lubna.abdelkhaleq@gmail.com>
2025-05-06 23:39:43 +0300

    scanf

---
+++ level 1/scanf/scanf.c
@@ -0,0 +1,184 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <unistd.h>
+
+int match_space(FILE *f)
+{
+    int c;
+
+    c = fgetc(f);
+    while (c != EOF)
+    {
+        if (!isspace(c))
+        {
+            ungetc(c, f); // put the character back into the file
+            break;
+        }
+        c = fgetc(f); // read the next character
+    }
+    return (0);
+}
+
+int match_char(FILE *f, char c)
+{
+    int i;
+
+    //i = fgetc(f);
+    while ((i = fgetc(f)) != EOF)
+    {
+        if (!isdigit(i))
+        {
+            ungetc(i, f);
+            break;
+        }
+    if (i == c)
+        return 1;
+    }
+    return (0);
+}
+
+int scan_char(FILE *f, va_list ap)
+{
+    int ch;
+    char *cp; // where we should store the result
+
+    ch = fgetc(f);
+    if (ch == EOF) // we do if condition for one time only because it is only 1 char that is being checked
+        return 0; // failed to read
+    cp = va_arg(ap, char *); // but char * is a string?
+    *cp = (char)ch;
+    return 1; // character was read and stored
+}
+
+int scan_int(FILE *f, va_list ap)
+{
+    int ch;
+    int value = 0;
+    int sign = 1;
+    int *ip; // always in the scan function you need a pointer to the thing in the function (int, char, string)
+
+    ch = fgetc(f);
+    while (isspace(ch))
+        ch = fgetc(f); // move to the next
+    if (ch == '-')
+    {
+        sign = -1;
+        ch = fgetc(f);
+    }
+    else if (ch == '+')
+        ch = fgetc(f);
+    if (!isdigit(ch))
+    {
+        if (ch != EOF)
+            ungetc(ch, f);
+        return (0);
+    }
+    while (isdigit(ch))
+    {
+        value = value * 10 + (ch - '0');
+        ch = fgetc(f);
+    }
+    if (ch != EOF)
+        ungetc(ch, f); // why not fgetc?
+    ip = va_arg(ap, int *); // can i do it in line 59?
+    *ip = value * sign;
+    return 1;
+}
+
+int scan_string(FILE *f, va_list ap)
+{
+    int ch;
+    char *sp = va_arg(ap, char *);
+    int i = 0;
+
+    ch = fgetc(f);
+    while (ch != EOF && isspace(ch))
+        ch = fgetc(f);
+    if (ch == EOF)
+        return (0); // why not ungetc?
+    do
+    {
+        sp[i] = ch;
+            i++;
+        ch = fgetc(f); // why do keep doing this?
+    } while (ch != EOF && !isspace(ch));
+
+    if (ch != EOF)
+        ungetc(ch, f);
+    sp[i] = '\0';
+    if (i > 0) //??
+        return (1);
+    else
+        return (0);
+}
+
+int match_conv(FILE *f, const char **format, va_list ap)
+{
+	switch (**format)
+	{
+		case 'c':
+			return scan_char(f, ap);
+		case 'd':
+			match_space(f);
+			return scan_int(f, ap);
+		case 's':
+			match_space(f);
+			return scan_string(f, ap);
+		case EOF:
+			return -1;
+		default:
+			return -1;
+	}
+}
+
+int ft_vfscanf(FILE *f, const char *format, va_list ap)
+{
+	int nconv = 0;
+
+	int c = fgetc(f);
+	if (c == EOF)
+		return EOF;
+	ungetc(c, f);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (match_conv(f, &format, ap) != 1)
+				break;
+			else
+				nconv++;
+		}
+		else if (isspace(*format))
+		{
+			if (match_space(f) == -1)
+				break;
+		}
+		else if (match_char(f, *format) != 1)
+			break;
+		format++;
+	}
+	if (ferror(f))
+		return (EOF);
+	return nconv;
+}
+
+int ft_scanf(const char *format, ...)
+{
+	va_list ap;
+    va_start (ap, format);
+	int ret = ft_vfscanf(stdin, format, ap);
+	va_end(ap);
+	return (ret);
+}
+
+int main (void)
+{
+    int x;
+    char str[100];
+    char str2[100];
+    ft_scanf("%d %s %s", &x, str, str2);
+    printf("%d %s %s\n", x, str, str2);
+}
