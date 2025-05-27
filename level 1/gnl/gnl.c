7eb230f83eef91c46ab86abc717426941af28818
Lubnabd <lubna.abdelkhaleq@gmail.com>
2025-05-27 17:01:44 +0300

    gnl

--- level 1/gnl/gnl.c
+++ level 1/gnl/gnl.c
@@ -1,123 +1,213 @@
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+#include "gnl.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-char	*ft_strchr(char *s, int c)
+int	ft_strlen(char *s)
 {
-	int i = 0;
-	if (!s)
-		return NULL;
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+int	ft_strchr(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
 	while (s[i])
 	{
-		if (s[i] == (char)c)
-			return s + i;
+		if (s[i] == c)
+			return (1);
 		i++;
 	}
-	return NULL;
+	return (0);
 }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	if (!dest || !src)
-		return NULL;
-	for (size_t i = 0; i < n; i++)
-		((char *)dest)[i] = ((char *)src)[i];
-	return dest;
-}
+	char	*res;
+	int		len1;
+	int		len2;
+	int		i;
+	int		j;
 
-size_t	ft_strlen(char *s)
-{
-	size_t ret = 0;
-	while (s && *s++)
-		ret++;
-	return ret;
-}
-
-int	str_append_mem(char **s1, char *s2, size_t size2)
-{
-	size_t size1 = ft_strlen(*s1);
-	char *tmp = malloc(size1 + size2 + 1);
-	if (!tmp)
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	res = malloc(len1 + len2 + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s1[i])
 	{
-		free(*s1);
-		*s1 = NULL;
-		return 0;
+		res[i] = s1[i];
+		i++;
 	}
-	if (*s1)
-		ft_memcpy(tmp, *s1, size1);
-	ft_memcpy(tmp + size1, s2, size2);
-	tmp[size1 + size2] = 0;
-	free(*s1);
-	*s1 = tmp;
-	return 1;
+	j = 0;
+	while (s2[j])
+		res[i++] = s2[j++];
+	res[i] = '\0';
+	return (res);
 }
 
-int	str_append_str(char **s1, char *s2)
+char	*set_next(char *buffer)
 {
-	return str_append_mem(s1, s2, ft_strlen(s2));
+	char	*line;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		buffer = NULL;
+		return (NULL);
+	}
+	line = malloc(ft_strlen(buffer) - i + 1);
+	if (!line)
+	{
+		free(buffer);
+		buffer = NULL;
+		return (NULL);
+	}
+	j = 0;
+	while (buffer[i] != '\0')
+		line[j++] = buffer[i++];
+	line[j] = '\0';
+	free(buffer); //static var
+	return (line);
 }
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+char	*set_line(char *buffer)
 {
-	if (!dest || !src)
-		return NULL;
-	//if (dest == src || n == 0)
-		//return dest;
-	if (dest < src)
-		return ft_memcpy(dest, src, n);
-	while (n--)
-		((char *)dest)[n] = ((char *)src)[n];
-	return dest;
+	char	*line;
+	int		i;
+	int		j;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+		i++;
+	line = malloc(i + 2);
+	if (!line)
+		return (NULL);
+	j = 0;
+	while (j < i + 2)
+		line[j++] = '\0';
+	i = 0;
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] != '\0' && buffer[i] == '\n')
+		line[i] = '\n';
+	return (line);
+}
+
+char	*join_str(char *buffer, char *buf, int byte_read)
+{
+	char	*temp;
+
+	if (byte_read == -1)
+	{
+		free(buf);
+		buf = NULL;
+		free(buffer);
+		return (NULL);
+	}
+	buf[byte_read] = '\0';
+	temp = ft_strjoin(buffer, buf);
+	if (!temp)
+	{
+		free(buf);
+		buf = NULL;
+		free(buffer);
+		return (NULL);
+	}
+	free(buffer);
+	return (temp);
+}
+
+char	*read_file(int fd, char *res)
+{
+	char	*buffer;
+	int		byte_read;
+
+	if (!res)
+	{
+		res = malloc(1);
+		res[0] = '\0';
+	}
+	if (!res)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+	{
+		free (res);
+		return (NULL);
+	}
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		res = join_str(res, buffer, byte_read);
+		if (!res)
+			return (NULL);
+		if (ft_strchr(buffer, '\n'))
+			break;
+	}
+	free (buffer);
+	return (res);
 }
 
 char	*get_next_line(int fd)
 {
-	static char b[BUFFER_SIZE + 1] = "";
-	char *ret = NULL;
-	char *tmp;
-	ssize_t read_ret;
-	read_ret = -1;
+	static char	*buffer;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return NULL;
-
-	tmp = ft_strchr(b, '\n');
-	while (!tmp)
+		return (NULL);
+	buffer = read_file(fd, buffer);
+	if (!buffer)
 	{
-		if (*b && !str_append_str(&ret, b))
-			return NULL;
-
-		read_ret = read(fd, b, BUFFER_SIZE);
-
-		if (read_ret < 0)
-		{
-			b[0] = 0;
-			if (ret)
-				free(ret);
-			return NULL;
-		}
-		else if (read_ret == 0)
-		{
-			if (ret && *ret)
-			{
-				b[0] = 0;
-				return ret;
-			}
-			free(ret);
-			return NULL;
-		}
-		b[read_ret] = 0;
-		tmp = ft_strchr(b, '\n');
+		free(buffer);
+		buffer = NULL;
+		return (NULL);
 	}
-	if (!str_append_mem(&ret, b, tmp - b + 1))
+	line = set_line(buffer);
+	if (!line)
 	{
-		free(ret);
-		return NULL;
+		free(buffer);
+		buffer = NULL;
+		return (NULL);
 	}
-	ft_memmove(b, tmp + 1, ft_strlen(tmp + 1) + 1);
-	return ret;
+	buffer = set_next(buffer);
+	return (line);
+} //195
+
+int main(void)
+{
+	int fd = open("test.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		perror("open");
+		return 1;
+	}
+
+	char *line;
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return 0;
 }
