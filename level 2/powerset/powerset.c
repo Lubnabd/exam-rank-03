9cb0cbccc7900e529860b95bf2f9e56fad4df712
Lubnabd <lubna.abdelkhaleq@gmail.com>
2025-06-11 15:34:28 +0300

    done

--- level 2/powerset/powerset.c
+++ level 2/powerset/powerset.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "powerset.h"
 
-void	print_subset(int *subset, int size)
+void print_subset(int *subset, int size)
 {
     int i = 0;
-    while ( i < size)
+    if (size == 0)
+        return ;
+    while (i < size)
     {
-        if (i == size - 1) //???
+        if ( i == size - 1)
             printf("%d", subset[i]);
         else
             printf("%d ", subset[i]);
@@ -16,37 +16,44 @@ void	print_subset(int *subset, int size)
     printf("\n");
 }
 
-void backtrack(int *set,  int *subset, int size, int index,
-    int subset_size, int current_sum, int target)
+void backtrack(int *set, int *subset, int size, int index, int sub_size, int current_sum, int target, int *found)
 {
-    if (index == size) // why not index + 1 == size? // did we consider subsetsize 1 or 0? because current sum is 0
+    if (index == size)
     {
-        if (current_sum == target) //? // 1 2 3 target is 5??
-            print_subset(subset, subset_size);
+        if (current_sum == target)
+        {
+            print_subset(subset, sub_size);
+            *found = 1;
+        }
         return ;
     }
-    backtrack(set, subset, size, index + 1, subset_size, current_sum, target); // from line 28 to 30?
-    subset[subset_size] = set[index];
-    backtrack(set, subset, size, index + 1, subset_size + 1, current_sum + set[index], target);
+    backtrack(set, subset, size, index + 1, sub_size, current_sum, target, found);
+    subset[sub_size] = set[index];
+    backtrack(set, subset, size, index + 1, sub_size + 1, current_sum + set[index], target, found);
 }
 
-int main (int argc, char ** argv)
+int main (int argc , char **argv)
 {
-    int set[argc - 2]; // how would the program know that the first two elements not the last?
+    int set[argc - 2]; // after we do the atoi
     int subset[argc - 2];
     int i = 0;
-    int j = 2; //argv? no
-    int size = argc - 2; // whats the point of having three int that equals the same?
+    int j = 2;
+    int size = argc - 2;
     int target = atoi(argv[1]);
+    int found = 0;
 
-    if (argc > 2)
+   if (argc > 2)
     {
-        while (j < argc)
+        while ( j < argc)
         {
             set[i] = atoi(argv[j]);
             i++;
             j++;
         }
-        backtrack(set, subset , size, 0, 0, 0, target);
+        backtrack(set, subset, size, 0 , 0, 0, target, &found);
+        if (!found)
+            printf("\n");
     }
+    else
+        return 1;
 }
