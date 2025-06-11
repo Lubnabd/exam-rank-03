4e4b32ea8ee6b684d4055b46e0613521655fdcf2
Lubnabd <lubna.abdelkhaleq@gmail.com>
2025-06-11 14:17:26 +0300

    done

--- level 2/permutations/permutations.c
+++ level 2/permutations/permutations.c
@@ -1,50 +1,39 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "perm.h"
 
 int ft_strlen(char *s)
 {
     int i = 0;
-
     while (s[i])
         i++;
-    return (i);
+    return i;
 }
 
 void ft_swap(char *a, char *b)
 {
-    char tmp;
-    tmp = *a;
+    char t;
+
+    t = *a;
     *a = *b;
-    *b = tmp;
+    *b = t;
 }
 
 char *order(char *s)
 {
-    int i = 0;
-    int len = ft_strlen(s);
+    int i;
     int swap;
+    int len = ft_strlen(s);
 
-    while (s[i] && s[i +1])
-    {
-        if (s[i] < s[i + 1])
-            i++;
-        else
-            break;
-    }
-    if ((i + 1) == len)
-        return (s);
     swap = 1;
     while (swap)
     {
-        swap = 0;
+        swap = 0; // assume nothing will need swapping
         i = 0;
-        while (s[i] && s[i + 1])
+        while (i < len - 1)
         {
             if (s[i] > s[i + 1])
             {
                 ft_swap(&s[i], &s[i + 1]);
-                swap = 1;
+                swap = 1; // A swap happened so we need another pass
             }
             i++;
         }
@@ -58,40 +47,42 @@ int ft_strchr(const char *s, char c)
     while (s[i])
     {
         if (s[i] == c)
-            return (1);
+            return 1;
         i++;
     }
-    return (0);
+    return 0;
 }
 
-void permutations(char *s, char *result, int pos)
+void permutations(char *s, char *res, int pos)
 {
     int i = 0;
-    if (result && (ft_strlen(result) == ft_strlen(s)))
+    if (res && (ft_strlen(res) == ft_strlen(s)))
     {
-        write (1, result, ft_strlen(result));
+        write (1, res, ft_strlen(res));
         write (1, "\n", 1);
     }
     while ( i < ft_strlen(s))
     {
-        if (!ft_strchr(result, s[i]))
+        if (!ft_strchr(res, s[i])) // check if character is already used
         {
-            result[pos] = s[i];
-            permutations (s, result, pos + 1);
-            result[pos] = '\0';
+            res[pos] = s[i];
+            permutations(s, res, pos + 1);
+            res[pos] = '\0';
         }
         i++;
     }
 }
 
-int main (int argc , char ** argv)
+int main (int argc , char **argv)
 {
-    char *s;
     char *res;
+    char *s;
 
     if (argc == 2)
     {
         res = calloc(ft_strlen(argv[1]), 1);
+        if (!res)
+            return (1);
         s = order(argv[1]);
         permutations(s, res, 0);
         free(res);
@@ -99,3 +90,5 @@ int main (int argc , char ** argv)
     else
         return 1;
 }
+
+// test with valgrind
