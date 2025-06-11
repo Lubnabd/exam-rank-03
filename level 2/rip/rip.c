5c8e97833aae19c07342ce282ee1382d6b8e8461
Lubnabd <lubna.abdelkhaleq@gmail.com>
2025-06-11 17:56:14 +0300

    done

--- level 2/rip/rip.c
+++ level 2/rip/rip.c
@@ -1,49 +1,48 @@
 #include "rip.h"
 
-int	ft_strlen(char *s)
+int ft_strlen(char *s)
 {
-	int	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
+    int i = 0;
+    while (s[i])
+        i++;
+    return i;
 }
 
-int good_closed(char *str, int n)
+int good_closed(char *s, int n)
 {
     int i = 0;
     int flag = 0;
-    while (i < n)
+    while ( i < n)
     {
-        if (str[i] == '(')
+        if (s[i] == '(')
             flag++;
-        else if (str[i] == ')')
+        if (s[i] == ')')
             flag--;
         if (flag == -1) // more ) than (
             return 0;
-        i++;
+    i++;
     }
-    if (flag == 0) // true all open brackets are matched
+    if (flag == 0)
         return 1;
-    return 0; // why did i do it twice?
+    else
+        return 0;
 }
 
 void check_rip(char *str, int n, int i, int *small, int lvl)
 {
     char tmp;
 
-    if (good_closed(str, n)) //??
+    if (good_closed(str, n))
     {
-        if (*small == -1 || lvl < *small) //???
+        if (*small == -1 || lvl < *small)
             *small = lvl;
     }
-    while ( i < n)
+    while (i < n)
     {
         tmp = str[i];
         str[i] = '_';
         lvl++;
-        check_rip(str, n, i + 1, small, lvl); //do i always do the +1 in recursion?
+        check_rip(str, n , i + 1, small, lvl);
         str[i] = tmp;
         lvl--;
         i++;
@@ -53,19 +52,20 @@ void check_rip(char *str, int n, int i, int *small, int lvl)
 void rip(char *str, int n, int i, int small, int lvl)
 {
     char tmp;
+
     if (lvl > small)
         return ;
     if (good_closed(str, n) && lvl == small)
     {
         write (1, str, ft_strlen(str));
-        write (1, "\n", 1);
+        write (1, " \n", 2);
         return ;
     }
     while ( i < n)
     {
         tmp = str[i];
-        lvl++;
         str[i] = '_';
+        lvl++;
         rip(str, n, i + 1, small, lvl);
         str[i] = tmp;
         lvl--;
@@ -73,9 +73,9 @@ void rip(char *str, int n, int i, int small, int lvl)
     }
 }
 
-int main (int argc , char ** argv)
+int main (int argc, char **argv)
 {
-    int small = -1; //?? also if it is the same as the *small in check_rip why is it an int here?
+    int small = -1;
     if (argc == 2)
     {
         check_rip(argv[1], ft_strlen(argv[1]), 0, &small, 0);
@@ -84,5 +84,3 @@ int main (int argc , char ** argv)
     else
         return 1;
 }
-
-
