893962bbc27735a553afc6869cfaf9c1f2f20dbb
Lubnabd <lubna.abdelkhaleq@gmail.com>
2025-06-18 21:00:14 +0300

    final

--- level 1/scanf/scanf2.c
+++ level 1/scanf/scanf2.c
@@ -1,18 +1,18 @@
-#include <stdio.h> // fgetc, ungetc
-#include <stdarg.h> // va_list
-#include <ctype.h> // isdigit , isspace
+#include <stdio.h>
+#include <stdarg.h>
+#include <ctype.h>
 
 int match_space(FILE *f)
 {
     int ch = fgetc(f);
-    while( ch != EOF)
+    while (ch != EOF)
     {
         if (!isspace(ch))
         {
-            ungetc(ch, f); // put the character back into the file
+            ungetc(ch, f);
             break ;
         }
-        ch = fgetc(f); // read the next character
+        ch = fgetc(f);
     }
     return 0;
 }
@@ -33,22 +33,22 @@ int scan_char(FILE *f, va_list ap)
     char *cp = va_arg(ap, char *);
 
     if (ch == EOF)
-        return 0; // failed to read
-    *cp = (char)ch;
+        return 0;
+    *cp = (char) ch;
     return 1;
 }
 
 int scan_int(FILE *f, va_list ap)
 {
-    int sign = 1;
     int value = 0;
+    int sign = 1;
+    int *ip = va_arg (ap , int *);
     int ch = fgetc(f);
-    int *ip = va_arg(ap, int *);
 
     if (ch == EOF)
         return 0;
     while (isspace(ch))
-        ch = fgetc(f); 
+        ch = fgetc(f); // fgetc Read until non-whitespace
     if (ch == '-')
     {
         sign = -1;
@@ -56,46 +56,41 @@ int scan_int(FILE *f, va_list ap)
     }
     else if (ch == '+')
         ch = fgetc(f);
-    
-    if (!isdigit(ch))
+    if (!isdigit(ch)) // if not while
     {
         ungetc(ch, f);
-        return 0;
+        return 0; // you keep forgetting 0
     }
     while (isdigit(ch))
     {
         value = value * 10 + (ch - '0');
         ch = fgetc(f);
     }
-    
     if (ch != EOF)
         ungetc(ch, f);
-
     *ip = value * sign;
     return 1;
 }
 
 int scan_string(FILE *f, va_list ap)
 {
+    int i = 0;
     int ch = fgetc(f);
     char *sp = va_arg(ap, char *);
-    int i = 0;
 
     while (ch != EOF && isspace(ch))
-        ch = fgetc(f);
+        ch = fgetc(f);; // fgetc here// You do ungetc once, not in a loop.
     if (ch == EOF)
         return 0;
-
     do
     {
         sp[i] = ch;
         i++;
         ch = fgetc(f);
     } while (ch != EOF && !isspace(ch));
-
     if (ch != EOF)
         ungetc(ch, f);
-    sp[i] = '\0';
+    sp[i] = '\0'; // this should be the last thing(here)
     return 1;
 }
 
@@ -153,21 +148,9 @@ int ft_vfscanf(FILE *f, const char *format, va_list ap)
 
 int ft_scanf(const char *format, ...)
 {
-    va_list ap;
-
-    va_start(ap, format);
-    int ret = ft_vfscanf(stdin, format, ap);
-    va_end(ap);
-    return (ret);
+	va_list ap;
+    va_start (ap, format);
+	int ret = ft_vfscanf(stdin, format, ap);
+	va_end(ap);
+	return (ret);
 }
-
-int main(void)
-{
-    int x;
-    char str[100];
-    char str2[200];
-    ft_scanf("%d %s %s", &x, str, str2);
-    printf("%d %s %s\n", x, str, str2);
-}
-
-// 108
