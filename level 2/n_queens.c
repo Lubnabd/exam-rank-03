378dd45dece054e1f4f73618d374e9dd32f32a36
Lubnabd <121336722+Lubnabd@users.noreply.github.com>
2025-06-29 12:04:02 +0300

    Update n_queens.c

--- level 2/n_queens.c
+++ level 2/n_queens.c
@@ -43,7 +43,7 @@ int main(int argc, char **argv)
 	int size = atoi(argv[1]);
 	int *board;
 
-	if (argc != 2 || size = < 1)
+	if (argc != 2 || size <= 0)
 		return 1;
 	
 	board = calloc(size, sizeof(int));
